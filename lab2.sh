2668726f43711124916304108a4ab3d4c15f5b8e
TRFish <sasha.brustovskiy2015@gmail.com>
2023-10-15 12:49:31 +1000

    [+] xomyak

--- lab2.sh
+++ lab2.sh
@@ -3,21 +3,24 @@
 # Основной цикл интерактивной оболочки
 command="help"
 while true; do
-    if [ $command = "help" ]
+    if [ $command = "help" || $command = "0" ]
     then
         echo "Таблица команд:"
         echo "info: Информация о пользователе и об ОС"
-        #echo ": "
+        echo "xomyak: Выводит список всех файлов из домашней дирректории (с правами)"
         #echo ": "
         echo
-    elif [ $command = "info" ]
+        
+    elif [ $command = "info" || $command = "1" ]
     then
         source /usr/lib/os-release
         echo "username: $USER"
         echo "release: $PRETTY_NAME ($BUILD_ID)"
-    elif [ $command = "3" ]
+
+    elif [ $command = "xomyak" || $command = "2" ]
     then
-        echo ih
+        ls -la ~
+
     fi
 
     echo -n "Command (help): "
