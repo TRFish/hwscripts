0ce194ee40dce0951052ef3c356ca5f99aeda9de
TRFish <sasha.brustovskiy2015@gmail.com>
2023-10-15 13:34:04 +1000

    [+] newfile, ping

--- lab2.sh
+++ lab2.sh
@@ -3,30 +3,54 @@
 # Основной цикл интерактивной оболочки
 command="help"
 while true; do
-    if [ $command = "help" || $command = "0" ]
+    if [ $command = "help" ] || [ $command = "0" ]
     then
         echo "Таблица команд:"
-        echo "info: Информация о пользователе и об ОС"
-        echo "xomyak: Выводит список всех файлов из домашней дирректории (с правами)"
-        #echo ": "
+        echo "0. help: Выводит таблицу всех комманд"
+        echo "1. info: Информация о пользователе и об ОС"
+        echo "2. xomyak: Выводит список всех файлов из домашней дирректории (с правами)"
+        echo "3. newfile: Создаёт новый файл с задаными правами"
+        echo "4. ping: Отправка пакетов"
         echo
-        
-    elif [ $command = "info" || $command = "1" ]
+
+    elif [ $command = "info" ] || [ $command = "1" ]
     then
         source /usr/lib/os-release
         echo "username: $USER"
+        #echo "hostname: $HOSTNAME"
         echo "release: $PRETTY_NAME ($BUILD_ID)"
 
-    elif [ $command = "xomyak" || $command = "2" ]
+    elif [ $command = "xomyak" ] || [ $command = "2" ]
     then
         ls -la ~
-
+    
+    elif [ $command = "newfile" ] || [ $command = "3" ]
+    then
+        echo -n "Введите название нового файла: "
+        read fname
+        echo -n "Введите права для нового файла (оставьте пустым для прав по-умолчанию): "
+        read frule
+        touch $fname
+        if [ -z $frule ]
+        then
+            echo -n # Испоьзуется вместо pass
+        else
+            chmod $frule $fname
+        fi
+    
+    elif [ $command = "ping" ] || [ $command = "4" ]
+    then
+        echo -n "Адрес: "
+        read address
+        echo -n "Кол-во пакетов: "
+        read packages
+        ping -c $packages $address
     fi
 
     echo -n "Command (help): "
     read command
-    #if [ command = " " ]
-    #then
-    #command="help"
-    #fi
+    if [ -z $command ]
+    then
+        command="help"
+    fi
 done
