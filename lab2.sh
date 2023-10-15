dd5f50a76306a9f79d2e5b7e32d82ad64424216f
TRFish <sasha.brustovskiy2015@gmail.com>
2023-10-15 12:28:48 +1000

    info command

--- lab2.sh
+++ lab2.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 # Основной цикл интерактивной оболочки
+command="help"
 while true; do
-    echo "Hi!"
+    if [ $command = "help" ]
+    then
+        echo "Таблица команд:"
+        echo "info: Информация о пользователе и об ОС"
+        #echo ": "
+        #echo ": "
+        echo
+    elif [ $command = "info" ]
+    then
+        source /usr/lib/os-release
+        echo "username: $USER"
+        echo "release: $PRETTY_NAME ($BUILD_ID)"
+    elif [ $command = "3" ]
+    then
+        echo ih
+    fi
+
+    echo -n "Command (help): "
+    read command
+    #if [ command = " " ]
+    #then
+    #command="help"
+    #fi
 done
